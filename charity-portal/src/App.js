e6920b1a3835c3a1121b2ec4a9951cb8c9c2c6dd
MonikaJ-J <monika@jarzembowska@gmail.com>
2021-06-03 16:30:25 +0200

    Initial commit

---
+++ charity-portal/src/App.js
@@ -0,0 +1,11 @@
+import './scss/main.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="reddish">Congrats</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ charity-portal/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "normalize.css";
+import "@fontsource/open-sans";
+import "@fontsource/merriweather";
+import './scss/main.scss';
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
